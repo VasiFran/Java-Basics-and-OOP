a22505cd15625b6bc7004762ae9a214d11cc2091
VasiFran <127098109+VasiFran@users.noreply.github.com>
2023-03-07 10:04:55 +0100

    Added javabasics 7: String

--- javabasics/_7/Main.java
+++ javabasics/_7/Main.java
@@ -16,6 +16,11 @@ public class Main {
     private static void exercise1() {
         System.out.println("\nExercise 1:");
         //Write your code here
+        String stringPractice = "I'm practicing with some STRINGS!";
+
+        System.out.println(stringPractice.toUpperCase() + " (In Upper Case)");
+        System.out.println(stringPractice.toLowerCase() + " (In Lower Case)");
+
     }
 
     /**
@@ -24,15 +29,15 @@ public class Main {
     private static void exercise2() {
         System.out.println("\nExercise 2:");
         // Change these
-        String firstName = "(your first name)";
-        String middleName = "(your middle name/any middle name)";
-        String lastName = "(your last name)";
+        String firstName = "Frank";
+        String middleName = "Adrian";
+        String lastName = "Swift";
 
-        System.out.println(firstName + " " + middleName + " " + lastName); // <--- Edit this line
+        System.out.println(firstName.toUpperCase() + " " + middleName.toLowerCase() + " " + lastName.toUpperCase()); // <--- Edit this line
     }
 
 
-     /**
+    /**
      * 3: Assign the value of 2 or more primitive types to our variable textFrom2Primitives
      *
      * Use the '+' operator
@@ -45,19 +50,32 @@ public class Main {
      * Experiment with these different types, how do an int and a double behave when added?
      *
      * What about a boolean and an int?
-      *
-      * "" + 1.5
+     *
+     * "" + 1.5
      *
      */
     private static void exercise3() {
         System.out.println("\nExercise 3:");
 
-        String textFrom2Primitives = ""; // <--- Edit this line
+        //String textFrom2Primitives = 23 + false; // <--- Edit this line
+        //String textFrom2Primitives = true + 3.16;
+        //String textFrom2Primitives = 'W' + false;
+        // ERROR: Can't apply "+" with bool and any primitive
+
+        //String textFrom2Primitives = 23 + 3.16; ERROR The program asks me for a double instead of a string
+
+        //String textFrom2Primitives = 'W' + 23 ; ERROR, asks me for an int (is he trying to sum the numeric value of 'K' with 23?)
+        //String textFrom2Primitives = 3.16 + 'W'; same error as above but asks for a double
+        int charPlusInt = 'W' + 23;
+        System.out.println(charPlusInt); //Yes, 'W' equals to 87, so + 23 goes to 110
+
+        String textFrom2Primitives = "Double: " + 3.16 + " | Int: " + 23 + " | Char: " + 'W' + " | Bool: " + false;
+        //If I use strings in works, just transforms every primitive in a string
         System.out.println(textFrom2Primitives);
     }
 
 
-     /**
+    /**
      * 4: Without editing the Strings below make the print statement print
      *
      *    1 2 3 4 5 6 7 8 9 with the correct spacing in between all the numbers
@@ -70,7 +88,7 @@ public class Main {
         String middleNumbers = "       4 5 6";
         String earlyNumbers = "1 2 3";
 
-        System.out.println(); // <--- Write your code here
+        System.out.println(earlyNumbers + " " + middleNumbers.trim() + " " + endNumbers); // <--- Write your code here
     }
 
 
