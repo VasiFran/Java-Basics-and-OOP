31bf68ee9385d18bc6c12501444f0cb82652c7ba
VasiFran <127098109+VasiFran@users.noreply.github.com>
2023-03-08 11:22:50 +0100

    Added javabasics Exercise 13: Arrays, Lists

--- javabasics/_13/Main.java
+++ javabasics/_13/Main.java
@@ -23,6 +23,22 @@ public class Main {
         ArrayList<Integer> numbers = new ArrayList<>();
 
         //Write your code here
+        numbers.add(12);
+        numbers.add(23);
+        numbers.add(47);
+        numbers.add(2);
+        numbers.add(117);
+        numbers.add(1);
+        numbers.add(83);
+        numbers.add(69);
+
+        System.out.println(numbers.get(0));
+
+        numbers.remove(0);
+        numbers.remove(0);
+        numbers.remove(0); // devo vedere se c'è un modo per rimuovere più di un indice dalla lista
+
+        System.out.println(numbers.get(0));
     }
 
     /**
@@ -38,8 +54,26 @@ public class Main {
      * menu item correspond
      */
     public static void exercise2() {
-        System.out.println("\nExercise 1:");
+        System.out.println("\nExercise 2:");
         //Write your code here
+        ArrayList<String> menuItems = new ArrayList<>();
+        ArrayList<Double> menuPrices = new ArrayList<>();
+
+        menuItems.add("Hamburger");
+        menuItems.add("Chicken");
+        menuItems.add("Salad");
+        menuItems.add("Fries");
+        menuItems.add("Milkshake");
+
+        menuPrices.add(3.99);
+        menuPrices.add(4.49);
+        menuPrices.add(4.79);
+        menuPrices.add(1.39);
+        menuPrices.add(2.19);
+
+        int menuOrder = 0;
+
+        System.out.println("Order n: " + (menuOrder+1) + ". Item: " + menuItems.get(menuOrder) + ". Price: " + menuPrices.get(menuOrder));
     }
 
     /**
@@ -58,5 +92,20 @@ public class Main {
         int[] items = new int[]{5, 10404, -234348, -53, 9, 91, 92, 34534, 12334, 1435};
 
         //Write your code here
+        int sum = 0;
+
+        for(int i = 0; i < items.length; i++ ){
+            sum += items[i];
+        }
+        System.out.println(sum);
+        // ho usato il ciclo for, mi sembrava la soluzione più comodo anche se ancora non è stato spiegato
+        // su Java, farò anche la versione senza for.
+
+        sum = 0;
+
+        sum = sum + items[0] + items[1] + items[2] + items[3] + items[4] +
+                    items[5] + items[6] + items[7] + items[8] + items[9];
+
+        System.out.println(sum);
     }
 }
